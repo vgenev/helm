a28453da14099c0bee22a28f64859fb0b78bbf7e
Unknown <shashikant.hirugade@softwaregroup.com>
2018-11-29 14:01:35 -0600

    moved the configuration to default.json and knexfile.js

---
+++ centralledger/chart-handler-timeout/files/knexfile.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const migrationsDirectory = '/opt/central-ledger/migrations'
+const seedsDirectory = '/opt/central-ledger/seeds'
+
+const Config = require('/opt/central-ledger/src/lib/config')
+
+module.exports = {
+    client: 'mysql',
+    connection: Config.DATABASE_URI,
+    migrations: {
+        directory: migrationsDirectory,
+        tableName: 'migration',
+        stub: `${migrationsDirectory}/migration.template`
+    },
+    seeds: {
+        directory: seedsDirectory,
+        loadExtensions: ['.js']
+    },
+    pool: { min: Config.MIN_CONNECTION_POOL, max: Config.MAX_CONNECTION_POOL }
+}
